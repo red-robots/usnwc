7471ae605d28c0dacf3ba7939e9585b8bf8d815f
Austin Crane <austincrane@gmail.com>
2019-10-08 17:06:14 -0400

    fix search

--- header-search.php
+++ header-search.php
@@ -1,6 +1,9 @@
+<?php global $is_tribe;
+if(!isset($is_tribe)) $is_tribe = false;?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <?php
        wp_enqueue_script('jquery');
@@ -14,7 +17,9 @@
 	 * Print the <title> tag based on what is being viewed.
 	 */
 	wp_title( '|', true, 'right' );
+	
 ?></title>
+
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -22,14 +27,18 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PLX2GN6');</script>
 <!-- End Google Tag Manager -->
+
+
 <?php wp_head();?>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="google-site-verification" content="e3L8kU2GBIb6wsJkRj2n6v1GDc-ajo6yg6IgX2xYl7w" />
 <link rel="profile" href="https://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-    <link rel="stylesheet" type="text/css"  href="<?php echo get_template_directory_uri(); ?>/style.css?v=<?php echo time();?>" />
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/flexslider.css?v=<?php echo time();?>" />
+    <!-- <link rel="stylesheet" type="text/css"  href="<?php echo get_template_directory_uri(); ?>/style.css?v=<?php //echo time();?>" /> -->
+    <link rel="stylesheet" type="text/css"  href="<?php echo get_template_directory_uri(); ?>/style.css?version=3.9.6" />
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/flexslider.css" />
+    <link rel="stylesheet" type="text/css"  href="<?php echo get_template_directory_uri(); ?>/css/animate.css" />
 <link rel="apple-touch-icon" sizes="57x57" href="/wp-content/uploads/2014/10/W_57x57.jpg" />
 <link rel="apple-touch-icon" sizes="72x72" href="/wp-content/uploads/2014/10/W_72x72.jpg" />
 <link rel="apple-touch-icon" sizes="114x114" href="/wp-content/uploads/2014/10/W_114x114.jpg" />
@@ -37,16 +46,16 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <link rel="icon" type="image/png" sizes="16x16" href="/wp-content/uploads/2015/09/W-Logo_16X16.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/wp-content/uploads/2015/09/W-Logo_32x32.png">
 <link rel="icon" type="image/png" sizes="96x96" href="/wp-content/uploads/2015/09/W-Logo_96X96.png">
-<link rel="stylesheet" href="/franklin-webfonts/franklingothicfs_condensed_macroman/stylesheet.css" type="text/css" charset="utf-8" />
-<link rel="stylesheet" href="/franklin-webfonts/franklingothicfs_condenseditalic_macroman/stylesheet.css" type="text/css" charset="utf-8" />
-<link rel="stylesheet" href="/franklin-webfonts/franklingothicfs_mediumcondensed_macroman/stylesheet.css" type="text/css" charset="utf-8" />
-<link rel="stylesheet" href="https://usnwc.org/wp-content/themes/usnwc/css/addtohomescreen.css">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/franklin-webfonts/franklingothicfs_condensed_macroman/stylesheet.css" type="text/css" charset="utf-8" />
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/franklin-webfonts/franklingothicfs_condenseditalic_macroman/stylesheet.css" type="text/css" charset="utf-8" />
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/franklin-webfonts/franklingothicfs_mediumcondensed_macroman/stylesheet.css" type="text/css" charset="utf-8" />
+<!-- <link rel="stylesheet" href="https://usnwc.org/wp-content/themes/usnwc/css/addtohomescreen.css"> -->
 <!--[if lt IE 9]><script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-<script src="<?php echo get_template_directory_uri(); ?>/js/addtohomescreen.min.js?"></script>
+<!-- <script src="<?php echo get_template_directory_uri(); ?>/js/addtohomescreen.min.js?"></script> -->
 <script src="https://use.fontawesome.com/4945cee666.js"></script>
-<script>
+<!-- <script>
 addToHomescreen({skipFirstVisit:true,maxDisplayCount:1});
-</script>
+</script> -->
 <script src="//platform.twitter.com/oct.js" type="text/javascript"></script>
 <script type="text/javascript">
 	twttr.conversion.trackPid('l4lpc');
@@ -100,21 +109,31 @@ if(is_category()){
 	echo $post->post_name;
 }
 ?>">
-<nav class="mobile">
-</nav>
+<!-- <nav class="mobile">
+</nav> -->
 <div class="page container">
 <div class="false header"><img class="false img" src="<?php echo get_template_directory_uri()."/images/falselogo.png";?>"></div>
 <header class="page">
 	<?php get_search_form();?>
+  <?php get_template_part('mobilenav'); ?>
 	<img class="showsearch" src="<?php echo get_template_directory_uri()."/images/search.png";?>">
-	<img class="showmenu hamburger" src="<?php echo get_template_directory_uri()."/images/hamburger-75x75.png";?>">
-    <nav>
+	<!-- <img class="showmenu hamburger" src="<?php echo get_template_directory_uri()."/images/hamburger-75x75.png";?>"> -->
+    <nav class="desktop">
    		<?php wp_nav_menu( array( 'theme_location' => 'header', 'container' => '' ) ); ?>
 	</nav>
     <a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img class="logo" src="<?php echo get_template_directory_uri()."/images/web_logo.png";?>" alt="U.S. National Whitewater Center Logo"></a>
 </header>
-<nav class="page breadcrumbs">
-	<li class='breadcrumb-item' id='breadcrumb-0'><a href='<?php echo home_url( '/' );?>'>Home</a></li>
-	<li class='breadcrumb-item'>  &raquo; Search</li>
-</nav>
+<?php if(!is_home()&&!$is_tribe) { ?>
+  <nav class="page breadcrumbs">
+    <?php 
+
+      if (is_page() || is_tax('product_cat') || is_singular(array('product','activity')) ) { 
+      	get_breadcrumbs();
+      } else { 
+      	bloglow_get_breadcrumb_navigation();
+      } 
+
+    //} ?>
+  </nav>
+<?php } ?>
 <main class="page">
