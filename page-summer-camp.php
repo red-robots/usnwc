108c900e48d79a11cccf0c424ad4922809f863b3
accrane <austincrane@Austins-MBP-2.attlocal.net>
2020-05-26 16:57:16 -0400

    replace with custom field

--- page-summer-camp.php
+++ page-summer-camp.php
@@ -7,8 +7,11 @@ get_header('page'); ?>
 
 
  <?php get_sidebar("banner");?>
-<?php if(have_posts()){ 
-   	the_post(); ?>
+<?php if(have_posts()){ the_post(); 
+
+		// reg link
+		$regLink = get_field('camp_registration_link');
+	?>
 	<?php if(in_array(get_field('sidebar'),array("top","both"),true)){
 		$sidebar="top";
 		get_template_part('sidebar');
@@ -34,14 +37,16 @@ get_header('page'); ?>
 					</header>
 				</div>
 			</a>
-			<a href="https://usnwc.theonlysky.com/ecomm3/products/view/5d8b3290/?addon=True&flow=Summer+Camp" target="_blank">
-				<div class="icon size-Default what-to-bring">
-			    	<img class="size-Default" src="<?php bloginfo('url'); ?>/wp-content/uploads/2014/10/what_to_bring.jpg">
-					<header>
-						<h2>Register</h2>
-					</header>
-				</div>
-			</a>
+			<?php if( $regLink ) { ?>
+				<a href="<?php echo $regLink; ?>" target="_blank">
+					<div class="icon size-Default what-to-bring">
+				    	<img class="size-Default" src="<?php bloginfo('url'); ?>/wp-content/uploads/2014/10/what_to_bring.jpg">
+						<header>
+							<h2>Register</h2>
+						</header>
+					</div>
+				</a>
+			<?php } ?>
 			<a href="<?php bloginfo('url'); ?>/learn/summer-camp/summer-camp-forms/" >
 				<div class="icon size-Default daily-activity-schedule">
 			    	<img class="size-Default" src="<?php bloginfo('url'); ?>/wp-content/uploads/2014/11/Activity_schedule_New.png">
