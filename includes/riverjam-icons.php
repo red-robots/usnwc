6c54f3584810963655d9c6905aac5620cd7e734e
accrane <austincrane@Austins-MBP-2.attlocal.net>
2020-07-17 14:36:31 -0400

    style icons

--- includes/riverjam-icons.php
+++ includes/riverjam-icons.php
@@ -34,3 +34,23 @@
 		</div>
 	</a>
 </div>
+
+<!-- Old code below that went into the wysiwyg -->
+<!-- <nav class="bookmark-links">
+<table>
+<tbody>
+<tr>
+<td><a href="#music"><img src="/wp-content/uploads/2016/05/Music.png" alt="music icon" /></a></td>
+<td><a href="#thursday-events"><img src="/wp-content/uploads/2016/05/ThusdayEvents.png" alt="thursday events icon" /></a></td>
+<td><a href="#completed"><img src="/wp-content/uploads/2015/09/Completed1.png" alt="completed icon" /></a></td>
+<td><a href="#playlists"><img src="/wp-content/uploads/2016/06/Playlist.png" alt="playlists icon" /></a></td>
+</tr>
+<tr>
+<td><a href="#music">Music</a></td>
+<td><a href="#thursday-events">Programming</a></td>
+<td><a href="#completed">Completed</a></td>
+<td><a href="#playlists">Playlists</a></td>
+</tr>
+</tbody>
+</table>
+</nav> -->
